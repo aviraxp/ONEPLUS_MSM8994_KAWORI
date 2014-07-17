08c37b0cdb408710c4e141a14bfea8cdacfcaba4
Lorenzo Pieralisi <Lorenzo.Pieralisi@arm.com>
2014-07-17 18:19:20 +0100

    arm64: kernel: add missing __init section marker to cpu_suspend_init
    Suspend init function must be marked as __init, since it is not needed
    after the kernel has booted. This patch moves the cpu_suspend_init()
    function to the __init section.
    
    Change-Id: I380b7013eb68ecf7bc0abe11dc5442142310f1b1
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Git-commit: 18ab7db6b749ac27aac08d572afbbd2f4d937934
    Git-repo: https://git.kernel.org/scm/linux/kernel/git/stable/linux-stable.git
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

--- arch/arm64/kernel/suspend.c
+++ arch/arm64/kernel/suspend.c
@@ -141,7 +141,7 @@ int __cpu_suspend(unsigned long arg, int (*fn)(unsigned long))
 extern struct sleep_save_sp sleep_save_sp;
 extern phys_addr_t sleep_idmap_phys;
 
-static int cpu_suspend_init(void)
+static int __init cpu_suspend_init(void)
 {
 	void *ctx_ptr;
 
