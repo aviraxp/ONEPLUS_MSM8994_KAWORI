e7a849a8129dff2ca6f115ce1e8a6a7bca058a78
Shuzhen Wang <shuzhenwang@google.com>
2016-09-08 11:12:39 -0700

    msm: camera: Update event_timestamp to be monotonic
    Switch event_timestamp to monotonic so that both MONOTONIC
    and BOOTTIME are available to user space
    
    Bug: 30471814
    Change-Id: Icdda2f7b082edca23148e062d0ec17de95dd4527

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -335,10 +335,14 @@ int msm_isp_get_clk_info(struct vfe_device *vfe_dev,
 static inline void msm_isp_get_timestamp(struct msm_isp_timestamp *time_stamp)
 {
 	struct timespec ts;
+
+	ktime_get_ts(&ts);
+	time_stamp->event_time.tv_sec = ts.tv_sec;
+	time_stamp->event_time.tv_usec = ts.tv_nsec/1000;
+
 	get_monotonic_boottime(&ts);
 	time_stamp->buf_time.tv_sec = ts.tv_sec;
 	time_stamp->buf_time.tv_usec = ts.tv_nsec/1000;
-	do_gettimeofday(&(time_stamp->event_time));
 }
 
 int msm_isp_subscribe_event(struct v4l2_subdev *sd, struct v4l2_fh *fh,
