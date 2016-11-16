5a0846a6d32eb5a045ed9a5ff62973b63b207386
andip71 <andreasp@gmx.de>
2016-11-16 07:10:51 +0100

    Boeffla ramfs: no default.prop patches anymore
    # Conflicts:
    #	anykernel/anykernel.sh

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -321,10 +321,6 @@ dump_boot;
 ############### Ramdisk customization start ###############
 
 # ramdisk changes
-backup_file default.prop;
-replace_string default.prop "ro.adb.secure=0" "ro.adb.secure=1" "ro.adb.secure=0";
-replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
-
 backup_file init.qcom.rc
 replace_line init.qcom.rc "group system inet net_admin wifi" "group system inet net_admin wifi radio net_raw";
 
