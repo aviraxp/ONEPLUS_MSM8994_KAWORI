1bf80002c5a06647746447871fe13cd88b8d0c14
James Christopher Adduono <jc@adduono.com>
2017-01-08 08:18:12 -0500

    Simplify mass storage gadget configuration
    Change-Id: I937084c54c1545d9a217d613dd9ac9d0202a3c70

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -3087,8 +3087,6 @@ static DEVICE_ATTR(ro, 0644, fsg_show_ro, fsg_store_ro);
 static DEVICE_ATTR(nofua, 0644, fsg_show_nofua, fsg_store_nofua);
 static DEVICE_ATTR(file, 0644, fsg_show_file, fsg_store_file);
 
-static struct device_attribute dev_attr_ro_cdrom =
-	__ATTR(ro, 0444, fsg_show_ro, NULL);
 static struct device_attribute dev_attr_file_nonremovable =
 	__ATTR(file, 0644, fsg_show_file, fsg_store_file);
 
@@ -3158,16 +3156,13 @@ static int create_lun_device(struct fsg_common *common,
 		}
 
 		rc = device_create_file(&curlun->dev,
-					curlun->cdrom
-				      ? &dev_attr_ro_cdrom
-				      : &dev_attr_ro);
-		if (rc)
-			goto error_luns;
-
-		rc = device_create_file(&curlun->dev,
 					curlun->removable
 				      ? &dev_attr_file
 				      : &dev_attr_file_nonremovable);
+		if (rc)
+			goto error_luns;
+
+		rc = device_create_file(&curlun->dev, &dev_attr_ro);
 		if (rc)
 			goto error_luns;
 
@@ -3387,10 +3382,7 @@ static void fsg_common_release(struct kref *ref)
 #endif
 			device_remove_file(&lun->dev, &dev_attr_cdrom);
 			device_remove_file(&lun->dev, &dev_attr_nofua);
-			device_remove_file(&lun->dev,
-					   lun->cdrom
-					 ? &dev_attr_ro_cdrom
-					 : &dev_attr_ro);
+			device_remove_file(&lun->dev, &dev_attr_ro);
 			device_remove_file(&lun->dev,
 					   lun->removable
 					 ? &dev_attr_file
