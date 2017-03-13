961f61fc9f7b6b9c81d15a1f0e5fa3d885006b65
John Dias <joaodias@google.com>
2017-03-13 14:58:19 -0700

    rcu: boost: avoid preemption before calling complete()
    Tracing showed that it was possible for a boosted
    thread to be descheduled with its priority de-boosted
    before it had signalled completion to the boosting thread.
    The fix here is to avoid preemption between releasing
    the mutex and calling complete().
    
    Bug: 34193533
    Change-Id: I598adb62ca675e2da5289b397f971127d4090092
    Signed-off-by: John Dias <joaodias@google.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    
    aviraxp: Backport for 3.10 (adjust context)

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -406,10 +406,18 @@ void rcu_read_unlock_special(struct task_struct *t)
 		}
 
 #ifdef CONFIG_RCU_BOOST
-		/* Unboost if we were boosted. */
+		/*
+		 * Unboost if we were boosted.
+		 * Disable preemption to make sure completion is signalled
+		 * without having the task de-scheduled with its priority
+		 * lowered (in which case we're left with no boosted thread
+		 * and possible RCU starvation).
+		 */
 		if (rbmp) {
+			preempt_disable();
 			rt_mutex_unlock(rbmp);
 			complete(&rnp->boost_completion);
+			preempt_enable();
 		}
 #endif /* #ifdef CONFIG_RCU_BOOST */
 
