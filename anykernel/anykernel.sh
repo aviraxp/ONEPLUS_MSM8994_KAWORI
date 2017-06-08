1d7001cb2bf0f1d325b1a07644e75fa359d8129a
Wang Han <416810799@qq.com>
2017-06-08 21:34:03 +0800

    anykernel: Don't let init scripts set default GPU power level
    This make the 27 mHz frequency back to stage.

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -322,7 +322,11 @@ dump_boot;
 
 # ramdisk changes
 backup_file init.qcom.rc
-replace_line init.qcom.rc "group system inet net_admin wifi" "group system inet net_admin wifi radio net_raw";
+replace_line init.qcom.rc "group system inet net_admin wifi" "     oneshot";
+
+backup_file init.qcom.power.rc
+remove_line init.qcom.power.rc "insmod /system/lib/modules/hcube.ko"
+remove_line "write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 5"
 
 ############### Ramdisk customization end ###############
 
