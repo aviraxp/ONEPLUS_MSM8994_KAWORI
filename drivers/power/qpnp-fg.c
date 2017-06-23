3baaad0844a9223ca6950994db62e405c5c1ce06
Wang Han <416810799@qq.com>
2017-06-22 23:03:41 -0700

    power: qpnp-fg: Remove some bogus wakeup sources
    These should not be called on normal usages, but they are. So
    just remove them.
    
    This should not break anything.
    
    Signed-off-by: Wang Han <416810799@qq.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -447,13 +447,10 @@ struct fg_chip {
 	struct power_supply	*batt_psy;
 	struct power_supply	*usb_psy;
 	struct power_supply	*dc_psy;
-	struct fg_wakeup_source	memif_wakeup_source;
 	struct fg_wakeup_source	profile_wakeup_source;
 	struct fg_wakeup_source	empty_check_wakeup_source;
 	struct fg_wakeup_source	resume_soc_wakeup_source;
 	bool			first_profile_loaded;
-	struct fg_wakeup_source	update_temp_wakeup_source;
-	struct fg_wakeup_source	update_sram_wakeup_source;
 /* david.liu@oneplus.tw,20160111  Rebase the fg driver to 8994L */
 #ifndef VENDOR_EDIT
 	bool			fg_restarting;
@@ -814,7 +811,6 @@ static int fg_req_and_wait_access(struct fg_chip *chip, int timeout)
 			pr_err("failed to set mem access bit\n");
 			return -EIO;
 		}
-		fg_stay_awake(&chip->memif_wakeup_source);
 	}
 
 wait:
@@ -841,7 +837,6 @@ static int fg_release_access(struct fg_chip *chip)
 
 	rc = fg_masked_write(chip, MEM_INTF_CFG(chip),
 			RIF_MEM_ACCESS_REQ, 0, 1);
-	fg_relax(&chip->memif_wakeup_source);
 	INIT_COMPLETION(chip->sram_access_granted);
 
 	return rc;
@@ -1922,7 +1917,6 @@ static void update_sram_data(struct fg_chip *chip, int *resched_ms)
 	int64_t temp;
 	int battid_valid = fg_is_batt_id_valid(chip);
 
-	fg_stay_awake(&chip->update_sram_wakeup_source);
 /* david.liu@oneplus.tw,20160111  Rebase the fg driver to 8994L */
 #ifndef VENDOR_EDIT
 	if (chip->fg_restarting)
@@ -2012,7 +2006,6 @@ resched:
 	} else {
 		*resched_ms = SRAM_PERIOD_NO_ID_UPDATE_MS;
 	}
-	fg_relax(&chip->update_sram_wakeup_source);
 }
 
 #define SRAM_TIMEOUT_MS			3000
@@ -2079,7 +2072,6 @@ static void update_temp_data(struct work_struct *work)
 		goto resched;
 #endif
 
-	fg_stay_awake(&chip->update_temp_wakeup_source);
 	if (chip->sw_rbias_ctrl) {
 		rc = fg_mem_masked_write(chip, EXTERNAL_SENSE_SELECT,
 				BATT_TEMP_CNTRL_MASK,
@@ -2136,17 +2128,11 @@ out:
 	}
 /* david.liu@oneplus.tw,20160111  Rebase the fg driver to 8994L */
 #ifndef VENDOR_EDIT
-	fg_relax(&chip->update_temp_wakeup_source);
-
 resched:
 #endif
 	schedule_delayed_work(
 		&chip->update_temp_work,
 		msecs_to_jiffies(TEMP_PERIOD_UPDATE_MS));
-/* david.liu@oneplus.tw,20160111  Rebase the fg driver to 8994L */
-#ifdef VENDOR_EDIT
-	fg_relax(&chip->update_temp_wakeup_source);
-#endif
 }
 
 static void update_jeita_setting(struct work_struct *work)
@@ -5328,10 +5314,7 @@ static void fg_cleanup(struct fg_chip *chip)
 	mutex_destroy(&chip->sysfs_restart_lock);
 	wakeup_source_trash(&chip->resume_soc_wakeup_source.source);
 	wakeup_source_trash(&chip->empty_check_wakeup_source.source);
-	wakeup_source_trash(&chip->memif_wakeup_source.source);
 	wakeup_source_trash(&chip->profile_wakeup_source.source);
-	wakeup_source_trash(&chip->update_temp_wakeup_source.source);
-	wakeup_source_trash(&chip->update_sram_wakeup_source.source);
 }
 
 static int fg_remove(struct spmi_device *spmi)
@@ -6168,14 +6151,8 @@ static int fg_probe(struct spmi_device *spmi)
 
 	wakeup_source_init(&chip->empty_check_wakeup_source.source,
 			"qpnp_fg_empty_check");
-	wakeup_source_init(&chip->memif_wakeup_source.source,
-			"qpnp_fg_memaccess");
 	wakeup_source_init(&chip->profile_wakeup_source.source,
 			"qpnp_fg_profile");
-	wakeup_source_init(&chip->update_temp_wakeup_source.source,
-			"qpnp_fg_update_temp");
-	wakeup_source_init(&chip->update_sram_wakeup_source.source,
-			"qpnp_fg_update_sram");
 	wakeup_source_init(&chip->resume_soc_wakeup_source.source,
 			"qpnp_fg_set_resume_soc");
 	mutex_init(&chip->rw_lock);
@@ -6404,10 +6381,7 @@ of_init_fail:
 	mutex_destroy(&chip->sysfs_restart_lock);
 	wakeup_source_trash(&chip->resume_soc_wakeup_source.source);
 	wakeup_source_trash(&chip->empty_check_wakeup_source.source);
-	wakeup_source_trash(&chip->memif_wakeup_source.source);
 	wakeup_source_trash(&chip->profile_wakeup_source.source);
-	wakeup_source_trash(&chip->update_temp_wakeup_source.source);
-	wakeup_source_trash(&chip->update_sram_wakeup_source.source);
 	return rc;
 }
 
