73113c41d757f11e7f511231a27398cd494186fc
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-25 11:40:26 +0200

    camera_v2: cpp: move default case to where it should be
    Change-Id: I2c88c09ee417a66c240743a5e17df8a841f509d9
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -2786,9 +2786,6 @@ STREAM_BUFF_END:
 		}
 		break;
 	}
-	default:
-		pr_err_ratelimited("invalid value: cmd=0x%x\n", cmd);
-		break;
 	case VIDIOC_MSM_CPP_IOMMU_ATTACH: {
 		if (cpp_dev->iommu_state == CPP_IOMMU_STATE_DETACHED) {
 			rc = iommu_attach_device(cpp_dev->domain,
@@ -2826,6 +2823,9 @@ STREAM_BUFF_END:
 		}
 		break;
 	}
+	default:
+		pr_err_ratelimited("invalid value: cmd=0x%x\n", cmd);
+		break;
 	}
 	mutex_unlock(&cpp_dev->mutex);
 	CPP_DBG("X\n");
