b9993ac603a3233421073f17d3a55d14e6506ed9
Wang Han <416810799@qq.com>
2017-10-21 22:11:50 +0800

    msm: kgsl: Don't wake up GPU on input events
    This will provide some additional powersaving, with a
    little performance lost.
    
    Based on CAF patch in MSM8998 branch.
    
    Signed-off-by: Wang Han <416810799@qq.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -926,14 +926,14 @@ int adreno_probe(struct platform_device *pdev)
 
 	adreno_input_handler.private = device;
 
-#ifdef CONFIG_INPUT
+//#ifdef CONFIG_INPUT
 	/*
 	 * It isn't fatal if we cannot register the input handler.  Sad,
 	 * perhaps, but not fatal
 	 */
-	if (input_register_handler(&adreno_input_handler))
-		KGSL_DRV_ERR(device, "Unable to register the input handler\n");
-#endif
+	//if (input_register_handler(&adreno_input_handler))
+		//KGSL_DRV_ERR(device, "Unable to register the input handler\n");
+//#endif
 out:
 	if (status) {
 		adreno_ringbuffer_close(adreno_dev);
