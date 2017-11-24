3ceaf997f8261eb81bfe236a859892e72143fdbc
Sultanxda <sultanxda@gmail.com>
2017-11-23 20:10:22 -0800

    Revert "misc: fpc1020: Increase fingerprintd nice value when screen is off"
    Eh, let's just give fingerprintd the highest priority.
    
    This reverts commit 388612e79e635d39cad30fadb57b73f9aa9314e4.
    
    aviraxp: Lack of MIN_NICE in 3.10

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -962,7 +962,7 @@ static void fpc1020_suspend_resume(struct work_struct *work)
 		set_fpc_irq(fpc1020, true);
 		set_fingerprintd_nice(0);
 	} else {
-		set_fingerprintd_nice(-1);
+		set_fingerprintd_nice(-20);
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL,
