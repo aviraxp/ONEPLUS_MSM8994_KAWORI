563dcec251862e87507cf2ea8fc9272848f05eb5
Mohammed Javid <mjavid@codeaurora.org>
2017-12-14 12:49:05 +0530

    msm: ipa: Change error value for set quota
    Currently IPA send common error -EFAULT to user space
    in case of IOCTL fails. Change error value for set quota
    based on error received from modem.
    
    Change-Id: Ib6ba487a186245ddf752cd08de12293af1ea1bb9
    Acked-by: Pooja Kumari <kumarip@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/platform/msm/ipa/rmnet_ipa_fd_ioctl.c
+++ drivers/platform/msm/ipa/rmnet_ipa_fd_ioctl.c
@@ -57,7 +57,7 @@ static dev_t device;
 
 static long wan_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	int retval = 0;
+	int retval = 0, rc = 0;
 	u32 pyld_sz;
 	u8 *param = NULL;
 
@@ -161,10 +161,14 @@ static long wan_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			break;
 		}
 
-		if (rmnet_ipa_set_data_quota(
-		(struct wan_ioctl_set_data_quota *)param)) {
+		rc = rmnet_ipa_set_data_quota(
+				(struct wan_ioctl_set_data_quota *)param);
+		if (rc != 0) {
 			IPAWANERR("WAN_IOC_SET_DATA_QUOTA failed\n");
-			retval = -EFAULT;
+			if (rc == -ENODEV)
+				retval = -ENODEV;
+			else
+				retval = -EFAULT;
 			break;
 		}
 
