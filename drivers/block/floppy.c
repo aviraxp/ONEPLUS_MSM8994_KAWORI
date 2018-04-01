9c93ba9771e5c55da564dfe770fadc183a8fdf09
BobZhome <BobZhome@cheerful.com>
2018-04-01 10:56:45 -0400

    [PATCH] floppy: Don't print kernel addresses to log in show_floppy CVE-2018-7273
    From Brian Belleville
    Date Tue, 20 Feb 2018 14:54:25 -0800
    
    Outputting kernel addresses will reveal the locations of kernel code
    and data. Change the cases in show_floppy that print
    fd_timer.work.func and fd_timeout.work.func to use the %pf format
    specifier, which will print the symbol name, like what is done for the
    other function pointers printed by show_floppy. No longer output the
    value of cont. The variable cont is a pointer that can hold the
    address of kernel global variables.
    
    Signed-off-by: Brian Belleville <bbellevi@uci.edu>
    
    https://lkml.org/lkml/2018/2/20/669

--- drivers/block/floppy.c
+++ drivers/block/floppy.c
@@ -1796,11 +1796,11 @@ static void show_floppy(void)
 	if (work_pending(&floppy_work))
 		pr_info("floppy_work.func=%pf\n", floppy_work.func);
 	if (delayed_work_pending(&fd_timer))
-		pr_info("delayed work.function=%p expires=%ld\n",
+		pr_info("delayed work.function=%pf expires=%ld\n",
 		       fd_timer.work.func,
 		       fd_timer.timer.expires - jiffies);
 	if (delayed_work_pending(&fd_timeout))
-		pr_info("timer_function=%p expires=%ld\n",
+		pr_info("timer_function=%pf expires=%ld\n",
 		       fd_timeout.work.func,
 		       fd_timeout.timer.expires - jiffies);
 
